787dbe45e3d7b40dc4163cd9773adc8983dee7c8
afeafe <karta2281085@gmail.com>
2025-01-11 23:11:53 +0800

    正在寫alip怎麼測試, 超難......

--- bipedal_floating_description/utils/trajatory_planning.py
+++ bipedal_floating_description/utils/trajatory_planning.py
@@ -53,53 +53,95 @@ class Trajatory:
             'rf' : np.vstack(( 0,  -0.1, z_sf, 0, 0, 0 )),
         }
 
-def ref_alip(self,stance,px_in_lf,px_in_rf,com_in_lf,com_in_rf,Com_ref_wf,L_ref_wf,R_ref_wf):
-    #ALIP
-    L_X_ref = L_ref_wf[0,0]
-    L_Y_ref = L_ref_wf[1,0]
-    L_Z_ref = L_ref_wf[2,0]
-
-    R_X_ref = R_ref_wf[0,0]
-    R_Y_ref = R_ref_wf[1,0]
-    R_Z_ref = R_ref_wf[2,0]
-
-    if stance == 1:
-        #取骨盆跟質心在位置上的差異(in_wf)
-        P_px_lf = np.reshape(copy.deepcopy(px_in_lf[0:3,0]),(3,1))
-        com2px_in_lf = P_px_lf - com_in_lf
-        O_wfL = np.array([[1,0,0],[0,1,0],[0,0,1]]) #直走&理想
-        com2px_in_wf = O_wfL@(com2px_in_lf)
-        #將質心軌跡更改成骨盆軌跡(in_wf)
-        P_X_ref = Com_ref_wf[0,0] + (com2px_in_wf[0,0])
-        P_Y_ref = Com_ref_wf[1,0] + (com2px_in_wf[1,0])
-        P_Z_ref = 0.55
-
-    elif stance == 0:
-        #取骨盆跟質心在位置上的差異(in_wf)
-        P_px_rf = np.reshape(copy.deepcopy(px_in_rf[0:3,0]),(3,1))
-        com2px_in_rf = P_px_rf - com_in_rf
-        O_wfR = np.array([[1,0,0],[0,1,0],[0,0,1]]) #直走&理想
-        com2px_in_wf = O_wfR@(com2px_in_rf)       
-        #將質心軌跡更改成骨盆軌跡(in_wf)
-        P_X_ref = Com_ref_wf[0,0] + (com2px_in_wf[0,0])
-        P_Y_ref = Com_ref_wf[1,0] + (com2px_in_wf[1,0])
-        P_Z_ref = 0.55
+class AlipTraj:
+    def __init__(self):
+        pass
     
-    #直走下預設為0
-    #骨盆姿態
-    P_Roll_ref = 0.0
-    P_Pitch_ref = 0.0
-    P_Yaw_ref = 0.0
-    #左腳腳底姿態
-    L_Roll_ref = 0.0
-    L_Pitch_ref = 0.0
-    L_Yaw_ref = 0.0
-    #右腳腳底姿態        
-    R_Roll_ref = 0.0
-    R_Pitch_ref = 0.0
-    R_Yaw_ref = 0.0
+    @classmethod
+    def plan(cls, stance, des_vx_com_in_cf_2T, t):
+        cf, sf = stance
+        
+        m = Config.MASS
+        H = Config.IDEAL_Z_COM_IN_WF
+        w = Config.OMEGA
+        T = Config.STEP_TIMELENGTH
+        W = Config.IDEAL_Y_RFTOLF_IN_WF
+        h = Config.STEP_HEIGHT
+        sign = -1 if sf == 'rf' else\
+                1
+        
+        #下兩步的理想角動量
+        des_Ly_com_in_cf_2T = m * des_vx_com_in_cf_2T * H
+        des_Lx_com_in_cf_2T = sign * ( 0.5*m*H*W ) * ( w*sinh(w*T) ) / ( 1+cosh(w*T) ) 
+        
+        #現在的質心ref
+        X0, Y0, p0_cf_in_wf, p0_sfTOcom_in_wf = cls.__getInitialData()
+        
+        var_x = cls.__getAlipMatA('x', t) @ X0
+        var_y = cls.__getAlipMatA('y', t) @ Y0
 
-    self.PX_ref = np.array([[P_X_ref],[P_Y_ref],[P_Z_ref],[P_Roll_ref],[P_Pitch_ref],[P_Yaw_ref]])
-    self.LX_ref = np.array([[L_X_ref],[L_Y_ref],[L_Z_ref],[L_Roll_ref],[L_Pitch_ref],[L_Yaw_ref]])
-    self.RX_ref = np.array([[R_X_ref],[R_Y_ref],[R_Z_ref],[R_Roll_ref],[R_Pitch_ref],[R_Yaw_ref]]) 
+        ref_p_com_in_cf = np.vstack(( var_x[0,0], var_y[0,0], H ))
+        ref_p_com_in_wf = ref_p_com_in_cf + p0_cf_in_wf
+        
+        #預測的下一步的初始角動量
+        pdc_Ly_com_in_cf_1T = var_x[1,0]
+        pdc_Lx_com_in_cf_1T = var_y[1,0]
+        
+        #下一步的軌跡點
+        pdc_xy_swTOcom_in_cf_T = np.vstack(( 
+            ( des_Ly_com_in_cf_2T - cosh(w*T)*pdc_Ly_com_in_cf_1T ) / ( m*H*w*sinh(w*T) ),
+            ( des_Lx_com_in_cf_2T - cosh(w*T)*pdc_Lx_com_in_cf_1T ) / -( m*H*w*sinh(w*T) )
+        ))
+        
+        #一步內擬和出的軌跡
+        ratio = t/T
+        
+        ref_p_sfTOcom_in_cf = np.vstack((
+            0.5*((1+cos(pi*ratio))*p0_sfTOcom_in_wf[:2] + (1-cos(pi*ratio))*pdc_xy_swTOcom_in_cf_T),
+            4*h*(ratio-0.5)**2 + (H-h)
+        ))
 
+        ref_p_sf_in_cf = ref_p_com_in_cf - ref_p_sfTOcom_in_cf
+        ref_p_sf_in_wf = ref_p_sf_in_cf + p0_cf_in_wf
+        
+        return {
+            'pel': ref_p_com_in_wf,
+               cf: p0_cf_in_wf,
+               sf: ref_p_sf_in_wf,
+        }
+
+    @classmethod
+    def __getInitialData(stance,t):
+        cf, sf = stance
+        if t == 0:
+            
+        X0, Y0, p0_cf_in_wf, p0_sfTOcom_in_wf = cls.__getInitialData()
+    
+    @staticmethod
+    def __getAlipMatA(axis:str, t:float) -> np.ndarray:
+        """理想ALIP動態矩陣"""
+        m = Config.MASS
+        H = Config.IDEAL_Z_COM_IN_WF
+        w = Config.OMEGA
+        
+        if axis == 'x':
+            return np.array([
+                [         cosh(w*t), sinh(w*t)/(m*H*w) ], 
+                [ m*H*w * sinh(w*t), cosh(w*t)         ]
+            ])
+        elif axis == 'y':
+            return np.array([
+                [          cosh(w*t), -sinh(w*t)/(m*H*w) ],
+                [ -m*H*w * sinh(w*t),  cosh(w*t)         ]
+            ])
+    
+if __name__ == '__main__':
+    t = 0
+    stance = ['lf', 'rf']
+    while  t<=20:
+        if t>Config.STEP_TIMELENGTH:
+            stance.reverse()
+            t=0
+        AlipTraj.plan(stance, 0.15, t)
+        t += Config.TIMER_PERIOD
+        
