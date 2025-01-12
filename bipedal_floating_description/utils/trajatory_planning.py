a147dc570a9b0e17c687a8cdd562f3aa2c28976d
afeafe <karta2281085@gmail.com>
2025-01-12 19:00:18 +0800

    擺動腳軌跡剩最後一點

--- bipedal_floating_description/utils/trajatory_planning.py
+++ bipedal_floating_description/utils/trajatory_planning.py
@@ -54,11 +54,22 @@ class Trajatory:
         }
 
 class AlipTraj:
-    def __init__(self):
-        pass
+    # def __init__(self):
+    #     t = 0
+    #     stance = ['lf', 'rf']
+    #     while  t<=20:
+    #         if t>Config.STEP_TIMELENGTH:
+    #             stance.reverse()
+    #             t=0
+    #         [
+    #             self.ref_p_com_in_wf,
+    #             self.p0_cf_in_wf,
+    #             self.ref_p_sf_in_wf,
+    #             self.ref_p_com_in_cf,
+    #         ] = AlipTraj.plan(stance, 0.15, t)
+    #         t += Config.TIMER_PERIOD
     
-    @classmethod
-    def plan(cls, stance, des_vx_com_in_cf_2T, t):
+    def plan(self, stance, des_vx_com_in_cf_2T, t):
         cf, sf = stance
         
         m = Config.MASS
@@ -67,81 +78,125 @@ class AlipTraj:
         T = Config.STEP_TIMELENGTH
         W = Config.IDEAL_Y_RFTOLF_IN_WF
         h = Config.STEP_HEIGHT
-        sign = -1 if sf == 'rf' else\
-                1
         
-        #下兩步的理想角動量
-        des_Ly_com_in_cf_2T = m * des_vx_com_in_cf_2T * H
-        des_Lx_com_in_cf_2T = sign * ( 0.5*m*H*W ) * ( w*sinh(w*T) ) / ( 1+cosh(w*T) ) 
+        #==========切換瞬間拿取初值==========#
+        var0, p0_cf_in_wf, p0_sfTOcom_in_wf = self.__getInitialData()
         
-        #現在的質心ref
-        X0, Y0, p0_cf_in_wf, p0_sfTOcom_in_wf = cls.__getInitialData()
         
-        var_x = cls.__getAlipMatA('x', t) @ X0
-        var_y = cls.__getAlipMatA('y', t) @ Y0
+        
+        #==========規劃質心軌跡與骨盆軌跡==========#
+        ref_p_com_in_cf, ref_p_com_in_wf = self.__plan_com(t, var0, p0_cf_in_wf)
+        
+        #==========規劃擺動腳軌跡==========#
+        
+        
+        
+        
+        
 
-        ref_p_com_in_cf = np.vstack(( var_x[0,0], var_y[0,0], H ))
-        ref_p_com_in_wf = ref_p_com_in_cf + p0_cf_in_wf
         
-        #預測的下一步的初始角動量
-        pdc_Ly_com_in_cf_1T = var_x[1,0]
-        pdc_Lx_com_in_cf_1T = var_y[1,0]
         
-        #下一步的軌跡點
-        pdc_xy_swTOcom_in_cf_T = np.vstack(( 
-            ( des_Ly_com_in_cf_2T - cosh(w*T)*pdc_Ly_com_in_cf_1T ) / ( m*H*w*sinh(w*T) ),
-            ( des_Lx_com_in_cf_2T - cosh(w*T)*pdc_Lx_com_in_cf_1T ) / -( m*H*w*sinh(w*T) )
-        ))
         
-        #一步內擬和出的軌跡
-        ratio = t/T
         
-        ref_p_sfTOcom_in_cf = np.vstack((
-            0.5*((1+cos(pi*ratio))*p0_sfTOcom_in_wf[:2] + (1-cos(pi*ratio))*pdc_xy_swTOcom_in_cf_T),
-            4*h*(ratio-0.5)**2 + (H-h)
-        ))
+        
 
         ref_p_sf_in_cf = ref_p_com_in_cf - ref_p_sfTOcom_in_cf
         ref_p_sf_in_wf = ref_p_sf_in_cf + p0_cf_in_wf
         
-        return {
-            'pel': ref_p_com_in_wf,
-               cf: p0_cf_in_wf,
-               sf: ref_p_sf_in_wf,
-        }
-
-    @classmethod
-    def __getInitialData(stance,t):
-        cf, sf = stance
-        if t == 0:
-            
-        X0, Y0, p0_cf_in_wf, p0_sfTOcom_in_wf = cls.__getInitialData()
-    
+    # @classmethod
+    # def __getInitialData(stance,t):
+    #     cf, sf = stance
+    #     if t == 0:
+    #         return
+    #     X0, Y0, p0_cf_in_wf, p0_sfTOcom_in_wf = cls.__getInitialData()
+        
     @staticmethod
-    def __getAlipMatA(axis:str, t:float) -> np.ndarray:
-        """理想ALIP動態矩陣"""
+    def matA(t):
         m = Config.MASS
         H = Config.IDEAL_Z_COM_IN_WF
         w = Config.OMEGA
-        
-        if axis == 'x':
-            return np.array([
-                [         cosh(w*t), sinh(w*t)/(m*H*w) ], 
-                [ m*H*w * sinh(w*t), cosh(w*t)         ]
-            ])
-        elif axis == 'y':
-            return np.array([
-                [          cosh(w*t), -sinh(w*t)/(m*H*w) ],
-                [ -m*H*w * sinh(w*t),  cosh(w*t)         ]
-            ])
+        return {
+            'x': np.array([
+                    [         cosh(w*t), sinh(w*t)/(m*H*w) ], 
+                    [ m*H*w * sinh(w*t), cosh(w*t)         ]   
+                ]),
+            'y': np.array([
+                    [          cosh(w*t), -sinh(w*t)/(m*H*w) ],
+                    [ -m*H*w * sinh(w*t),  cosh(w*t)         ]
+                ])
+        }
     
-if __name__ == '__main__':
-    t = 0
-    stance = ['lf', 'rf']
-    while  t<=20:
-        if t>Config.STEP_TIMELENGTH:
-            stance.reverse()
-            t=0
-        AlipTraj.plan(stance, 0.15, t)
-        t += Config.TIMER_PERIOD
+    @staticmethod
+    def __plan_com(self, t, var0: dict[str, np.ndarray], p0_cf_in_wf: np.ndarray):
+        H = Config.IDEAL_Z_COM_IN_WF
+        A = self.matA(t)
+        
+        #state equation
+        var = {
+            'x': A['x'] @ var0['x'],
+            'y': A['y'] @ var0['y'],
+        }
+        
+        #質心在cf的軌跡
+        ref_p_com_in_cf = np.vstack(( var['x'][0,0], var['y'][0,0], H ))
+        
+        #質心在wf的軌跡(理論直走的cf和wf姿態同向)
+        ref_p_com_in_wf = ref_p_com_in_cf + p0_cf_in_wf
+        
+        #參考的角動量(cf腳踝要控)
+        # ref_L_com_in_cf = np.vstack(( var['y'][1,0], var['x'][1,0] ))
+        
+        return ref_p_com_in_cf, ref_p_com_in_wf
+        
+    def __plan_sf(self, t, stance, des_vx_com_in_cf_2T, var0: dict[str, np.ndarray], p0_sfTOcom_in_wf):
+        cf, sf = stance
+        
+        m = Config.MASS
+        H = Config.IDEAL_Z_COM_IN_WF
+        W = Config.IDEAL_Y_RFTOLF_IN_WF
+        h = Config.STEP_HEIGHT
+        w = Config.OMEGA
+        T = Config.STEP_TIMELENGTH
+        
+        
+        sign_Lx = -1 if sf == 'rf' else\
+                   1
+                
+        A = self.matA(t)
+        
+        #求出下兩步的理想角動量
+        des_L_com_in_cf_2T = {
+            'y': m * des_vx_com_in_cf_2T * H,
+            'x': sign_Lx * ( 0.5*m*H*W ) * ( w*sinh(w*T) ) / ( 1+cosh(w*T) ) 
+        }
+        
+        #下步落地的角動量
+        ref_L_com_in_cf_1T = {
+            'y': ( A['x'] @ var0['x'] ) [1, 0],
+            'x': ( A['y'] @ var0['y'] ) [1, 0]
+        }
+        
+        #下步落地向量
+        ref_xy_swTOcom_in_cf_T = np.vstack(( 
+            ( des_L_com_in_cf_2T['y'] - cosh(w*T)*ref_L_com_in_cf_1T['y'] ) /  ( m*H*w*sinh(w*T) ),
+            ( des_L_com_in_cf_2T['x'] - cosh(w*T)*ref_L_com_in_cf_1T['x'] ) / -( m*H*w*sinh(w*T) )
+        ))
+        
+        #一步內擬合出的軌跡
+        ratio = t/T
+        theta = cos(pi * ratio)
+        angularMove = lambda a, x1, x0, a1, a0:\
+            x0 + (x1-x0)/(a1-a0) * (a-a0)
+        parabolicMove = lambda a, x1, x0, r1, r0:\
+            
+        
+        ref_p_sfTOcom_in_cf = np.vstack((
+            angularMove(theta, *[ ref_xy_swTOcom_in_cf_T, p0_sfTOcom_in_wf[:2] ], *[-1, 1] ),
+            
+            4*h*(ratio-0.5)**2 + (H-h)
+        ))
+        
+        
+
+    
         
