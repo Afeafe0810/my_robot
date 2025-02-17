5f142f90e1d8ff5d55a601b1e75d4cbda7b9fe1e
Afeafe0810 <karta2281085@gmail.com>
2025-02-17 15:23:40 +0800

    > 刪除一堆舊檔案(反正都在版本裡了), alip_test直接把Config內容移進去

--- bipedal_floating_description/alip軌跡驗證/alip_test.py
+++ bipedal_floating_description/alip軌跡驗證/alip_test.py
@@ -3,10 +3,58 @@ import pandas as pd
 from math import cosh, sinh, cos, sin, pi
 import csv
 #================ import library ========================#
-from utils.config import Config
-from utils.frame_kinermatic import RobotFrame
+
+# from utils.frame_kinermatic import RobotFrame
+#========================================================#
+import numpy as np
+#================ import library ========================#
+
 #========================================================#
 
+class Config:
+    '''機器人的參數、常數'''
+    
+    #取樣頻率
+    TIMER_PERIOD = 0.01
+    
+    #訂閱到的關節順序，是錯的
+    JNT_ORDER_SUB = (
+        'L_Hip_Z', 'L_Hip_Y', 'L_Knee_Y', 'L_Ankle_Y', 
+        'L_Ankle_X', 'R_Hip_X', 'R_Hip_Z', 'R_Knee_Y', 
+        'R_Hip_Y', 'R_Ankle_Y', 'L_Hip_X', 'R_Ankle_X'
+    )
+
+    #實際上的關節順序
+    JNT_ORDER_LITERAL = [
+        'L_Hip_X', 'L_Hip_Z', 'L_Hip_Y', 'L_Knee_Y', 
+        'L_Ankle_Y', 'L_Ankle_X', 'R_Hip_X', 'R_Hip_Z', 
+        'R_Hip_Y', 'R_Knee_Y', 'R_Ankle_Y', 'R_Ankle_X'
+    ]
+    
+    #URDF檔的路徑
+    ROBOT_MODEL_DIR = "/home/ldsc/ros2_ws/src/bipedal_floating_description/urdf"
+    
+    #骨盆相對base的座標
+    P_PEL_IN_BASE = np.vstack(( 0, 0, 0.598 ))
+    
+    #單腳支撐時間?
+    DDT = 2
+    
+    #行走每步時間?
+    STEP_TIMELENGTH = 0.5
+    
+    #機器人的物理模型
+    MASS = 9
+    IDEAL_Z_COM_IN_WF = 0.45
+    IDEAL_Y_RFTOLF_IN_WF = 0.2
+    GC = 9.81
+    OMEGA = ( GC / IDEAL_Z_COM_IN_WF )**0.5
+    STEP_HEIGHT = 0.02
+    
+    #腳踝關節限制
+    FOOT_WIDTH = 0.04
+    ANKLE_LIMIT = MASS * GC * FOOT_WIDTH #3.53 Nm
+    
 class AlipTraj:
     def __init__(self):
         self.t = 0.0
@@ -25,7 +73,7 @@ class AlipTraj:
             'rf': np.vstack(( 0, -0.1, 0 ))
         }
     
-    def plan(self, frame: RobotFrame, stance:list, des_vx_com_in_wf_2T ):
+    def plan(self, frame, stance:list, des_vx_com_in_wf_2T ):
         # input('go')
         
         
