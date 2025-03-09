1a88c24a866ded8fcb98e9cdd9c3837618cd45de
Afeafe0810 <karta2281085@gmail.com>
2025-03-09 18:52:10 +0800

    <refct> 改善命名

--- bipedal_floating_description/utils/motion_planning.py
+++ bipedal_floating_description/utils/motion_planning.py
@@ -62,14 +62,14 @@ class LeftLegBalance:
         self.t : float = 0.0
         
     def plan(self):
-        T = Config.DDT
+        TL = Config.DDT
                 
         #==========線性移動==========#
         linearMove = lambda x0, x1, t0, t1:\
             np.clip(x0 + (x1-x0) * (self.t-t0)/(t1-t0), x0, x1 )
             
-        y_pel = linearMove(*[0, 0.09], *[0*T, 0.5*T])
-        z_sf  = linearMove(*[0, Config.STEP_HEIGHT], *[1*T, 1.1*T])
+        y_pel = linearMove(*[0, 0.09], *[0*TL, 0.5*TL])
+        z_sf  = linearMove(*[0, Config.STEP_HEIGHT], *[1*TL, 1.1*TL])
         
         if self.t < 2 * Config.DDT:
             print(f"LeftLegBalance.t = {self.t:.2f}")
