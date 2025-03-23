0e6be8eef3b49b1077677622ac4c50177c639fe4
Afeafe0810 <karta2281085@gmail.com>
2025-03-24 00:23:53 +0800

    <bug> 擺動腳踝順序有誤

--- bipedal_floating_description/torque_control/ankle_control.py
+++ bipedal_floating_description/torque_control/ankle_control.py
@@ -12,7 +12,7 @@ def anklePD_ctrl(frame: RobotFrame, sf: str):
         'lf': frame.r_lf_to_wf,
         'rf': frame.r_rf_to_wf
     }
-    ayx_sf_in_wf = frame.rotMat_to_euler(r_ft_to_wf[sf]) [1:]
+    ayx_sf_in_wf = frame.rotMat_to_euler(r_ft_to_wf[sf]) [1::-1]
     #TODO 摩擦力看要不要加
     torque_ankle_sf = 0.1 * ( ref_jp - ayx_sf_in_wf ) # HACK 現在只用P control
     
