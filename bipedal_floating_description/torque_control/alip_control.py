22a445048b94f8561a3d423d4b7f9682bf5a3e64
Afe_lab <karta2281085@gmail.com>
2025-05-28 19:09:21 +0800

    > 有問題，不知道改了什麼現在平衡不起來

--- bipedal_floating_description/torque_control/alip_control.py
+++ bipedal_floating_description/torque_control/alip_control.py
@@ -22,45 +22,68 @@ class _Abstract_Alip_EstimateBias:
         # self.var_e: NDArray = np.vstack((0.02, 0))
         self.bias_e: float = 0
     
-    def ctrl(self, frame: RobotFrame, stance: list[str], stance_past: list[str], var: NDArray, ref_var: NDArray) -> float:
+    def ctrl(self, frame: RobotFrame, stance: list[str], stance_past: list[str], mea_pel: float, ref_var: NDArray) -> float:
         """回傳支撐腳腳踝扭矩"""
         if stance != stance_past or self.is_ctrl_first_time:
             self.is_ctrl_first_time = False
-            self.var_e = var
+            self.var_e = np.vstack((mea_pel, 0))
 
         #==========全狀態回授(飽和)==========# HACK 用估測的骨盆位置來進行回授
         _u = -self.K @ (self.var_e + np.vstack((self.bias_e, 0))- ref_var) 
         u = np.clip(_u, -self.limit, self.limit)
         
         #==========估測回授==========#
-        err_e = var[0, 0] - self.var_e[0, 0] - self.bias_e
+        err_e = mea_pel - self.var_e[0, 0] - self.bias_e
         self.var_e = self.A @ self.var_e + self.B * u + self.L * err_e
         self.bias_e = self.bias_e + self.L_bias * err_e
         
         return -u
 
-# class _Abstract_Alip_NoEstimate:
-#     def __init__(self, A: NDArray, B: NDArray, K: NDArray, L: NDArray, L_bias: NDArray):
-#         self.is_ctrl_first_time = True
+class _Abstract_Alip_NoEstimate:
+    def __init__(self, A: NDArray, B: NDArray, K: NDArray, L: NDArray, L_bias: NDArray, limit: float):
+        self.is_ctrl_first_time = True
         
-#         #狀態矩陣
-#         self.A  = A
-#         self.B  = B
-#         self.K  = K
+        #狀態矩陣
+        self.A  = A
+        self.B  = B
+        self.K  = K
+        self.limit = limit
     
-#     def ctrl(self, stance: list[str], stance_past: list[str], var: NDArray, ref_var: NDArray, limit: float) -> float:
-#         """回傳支撐腳腳踝扭矩"""
-#         if stance != stance_past or self.is_ctrl_first_time:
-#             self.is_ctrl_first_time = False
-#             self.var_e = var
+    def ctrl(self, frame: RobotFrame, stance: list[str], stance_past: list[str], var: NDArray, ref_var: NDArray) -> float:
+        """回傳支撐腳腳踝扭矩"""
+        if stance != stance_past or self.is_ctrl_first_time:
+            self.is_ctrl_first_time = False
+            self.var_e = var
 
-#         #==========全狀態回授(飽和)==========#
-#         _u = -self.K @ (var - ref_var) 
-#         u = np.clip(_u, -limit, limit)
+        #==========全狀態回授(飽和)==========#
+        _u = -self.K @ (var - ref_var) 
+        u = np.clip(_u, -self.limit, self.limit)
         
-#         return -u
+        return -u
     
-class AlipX(_Abstract_Alip_EstimateBias):
+# class AlipX(_Abstract_Alip_EstimateBias):
+#     def __init__(self):
+#         super().__init__(
+#             A = np.array([[1, 0.00247], [0.8832, 1]]),
+#             B = np.vstack((0, 0.01)),
+#             K = np.array([[150,15.0198]]),
+#             L = np.vstack((1.656737, 24.448707)),
+#             L_bias = -1.238861,
+#             limit = Config.ANKLE_AY_LIMIT
+#         )
+    
+#     def ctrl(self, frame: RobotFrame, stance: list[str], stance_past: list[str], mea_pel: float, ref_var: NDArray) -> float:
+#         ref_var = np.vstack((0.005, 0))
+#         mea_pel = frame.p_pel_in_wf[0, 0] - frame.p_lf_in_wf[0, 0]
+        
+#         if self.is_ctrl_first_time:
+#             self.is_ctrl_first_time = False
+#             self.var_e = np.vstack((mea_pel, 0))
+            
+#         output = super().ctrl(frame, stance, stance_past, mea_pel, ref_var)
+#         return output
+
+class AlipX(_Abstract_Alip_NoEstimate):
     def __init__(self):
         super().__init__(
             A = np.array([[1, 0.00247], [0.8832, 1]]),
@@ -70,17 +93,6 @@ class AlipX(_Abstract_Alip_EstimateBias):
             L_bias = -1.238861,
             limit = Config.ANKLE_AY_LIMIT
         )
-    
-    def ctrl(self, frame: RobotFrame, stance: list[str], stance_past: list[str], var: NDArray, ref_var: NDArray) -> float:
-        ref_var = np.vstack((0.005, 0))
-        var = np.vstack((frame.p_pel_in_wf[0, 0] - frame.p_lf_in_wf[0, 0], 0))
-        
-        if self.is_ctrl_first_time:
-            self.is_ctrl_first_time = False
-            self.var_e = var
-            
-        output = super().ctrl(frame, stance, stance_past, var, ref_var)
-        return output
             
 # class AlipY(_Abstract_Alip_NoEstimate):
 #     def __init__(self):
@@ -120,8 +132,9 @@ class AlipY1(_Abstract_Alip_EstimateBias):
         )
         self.bias_e = 0.02
 
-    def ctrl(self, frame: RobotFrame, stance: list[str], stance_past: list[str], var: NDArray, ref_var: NDArray) -> float:
+    def ctrl(self, frame: RobotFrame, stance: list[str], stance_past: list[str], mea_pel: float, ref_var: NDArray) -> float:
         ref_var = np.vstack((0.005, 0))
+        mea_pel = frame.p_pel_in_wf[0, 0] - frame.p_lf_in_wf[0, 0]
         
         if self.is_ctrl_first_time:
             self.is_ctrl_first_time = False
@@ -131,7 +144,7 @@ class AlipY1(_Abstract_Alip_EstimateBias):
             'com_e': self.var_e[0, 0],
             'com': frame.p_com_in_wf[0, 0] - frame.p_lf_in_wf[0, 0],
             'pel_e': self.var_e[0, 0] + self.bias_e,
-            'pel': frame.p_pel_in_wf[0, 0] - frame.p_lf_in_wf[0, 0],
+            'pel': mea_pel,
             'ref_var': ref_var[0, 0]
         })
         print(data)
@@ -140,5 +153,5 @@ class AlipY1(_Abstract_Alip_EstimateBias):
         ROS.publishers.pel_e.publish([data['pel_e']])
         ROS.publishers.pel.publish([data["pel"]])
         
-        output = super().ctrl(frame, stance, stance_past, var, ref_var)
+        output = super().ctrl(frame, stance, stance_past, mea_pel, ref_var)
         return output
