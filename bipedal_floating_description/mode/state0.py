321ff2f625776dd76cbcfe6bcc8dea170a4b019b
Afe_Book <karta2281085@gmail.com>
2025-06-29 21:54:44 +0800

    >1

--- bipedal_floating_description/mode/state0.py
+++ bipedal_floating_description/mode/state0.py
@@ -5,27 +5,26 @@ from typing import Literal
 from bipedal_floating_description.utils.robot_model import RobotModel
 #========================================================#
 
+def gravity(model_gravity: dict[str, NDArray], end_in_pf: dict[Literal['lf', 'rf', 'pel'], NDArray]) -> NDArray:
+    # 根據骨盆位置來判斷重心腳
+    end = end_in_pf
+    y_ftTOpel = {
+        'lf': abs(end['lf'][1] - end['pel'][1]),
+        'rf': abs(end['rf'][1] - end['pel'][1])
+    }
+    
+    gf = 'lf' if y_ftTOpel['lf'] <= y_ftTOpel['rf'] else 'rf'
+    
+    # 再根據重心腳距離權重
+    return model_gravity[gf] * (1 - y_ftTOpel[gf]/0.1) + model_gravity['from_both_single_ft'] * (y_ftTOpel[gf]/0.1)
+
 class State0:
     ref_jp = np.zeros(12)
     kp = np.array([ 2, 2, 4, 6, 6, 4 ]*2)
     
     def __init__(self, jp: NDArray, model_gravity: dict[str, NDArray], end_in_pf: dict[Literal['lf', 'rf', 'pel'], NDArray]):
         self.jp = jp.flatten()
-        self.tauG = self.gravity(model_gravity, end_in_pf)
+        self.tauG = gravity(model_gravity, end_in_pf)
         
-    @staticmethod
-    def gravity(model_gravity: dict[str, NDArray], end_in_pf: dict[Literal['lf', 'rf', 'pel'], NDArray]) -> NDArray:
-        # 根據骨盆位置來判斷重心腳
-        end = end_in_pf
-        y_ftTOpel = {
-            'lf': abs(end['lf'][1] - end['pel'][1]),
-            'rf': abs(end['rf'][1] - end['pel'][1])
-        }
-        
-        gf = 'lf' if y_ftTOpel['lf'] <= y_ftTOpel['rf'] else 'rf'
-        
-        # 再根據重心腳距離權重
-        return model_gravity[gf] * (1 - y_ftTOpel[gf]/0.1) + model_gravity['from_both_single_ft'] * (y_ftTOpel[gf]/0.1)
-            
     def ctrl(self):
         return self.kp * (self.ref_jp - self.jp) + self.tauG
